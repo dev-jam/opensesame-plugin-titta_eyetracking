fb4b75e352780b3315020e6ddb89051c91c82a9e
dev-jam <mail@bobrosbag.nl>
2023-07-04 23:33:03 +0200

    correction pypi

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_readme():
 
 setup(
     name='opensesame-plugin-titta_eyetracking',
-    version='0.2.0',
+    version='0.2.1',
     description='Titta Eye Tracking plugin for OpenSesame',
     long_description=get_readme(),
     long_description_content_type='text/markdown',
