f2a8cfd4274f1da474cdf26c478f0038f77c67a0
dev-jam <mail@bobrosbag.nl>
2023-08-30 23:37:58 +0200

    bump version

--- opensesame_plugins/titta/titta_send_message/__init__.py
+++ opensesame_plugins/titta/titta_send_message/__init__.py
@@ -15,6 +15,6 @@ controls = [
         "label": "<small><b>Note:</b> Titta Init item at the begin of the experiment is needed for initialization of the Eye Tracker</small>"
     }, {
         "type": "text",
-        "label": "<small>Titta Eye Tracking version 1.0.1</small>"
+        "label": "<small>Titta Eye Tracking version 1.0.2</small>"
     }
 ]
