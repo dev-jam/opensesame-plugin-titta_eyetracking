b2d6d30784d4ca4655f43f4250609a8571bab82e
dev-jam <mail@bobrosbag.nl>
2024-01-11 11:15:54 +0100

    bump version

--- opensesame_plugins/titta/titta_init/__init__.py
+++ opensesame_plugins/titta/titta_init/__init__.py
@@ -63,6 +63,6 @@ To minimize the risk of missing samples, the current repository uses TittaPy (pi
         "label": "<small><b>Note:</b> Titta Init item at the begin of the experiment is needed for initialization of the Eye Tracker</small>"
     }, {
         "type": "text",
-        "label": "<small>Titta Eye Tracking version 2.0.0</small>"
+        "label": "<small>Titta Eye Tracking version 2.1.0</small>"
     }
 ]
