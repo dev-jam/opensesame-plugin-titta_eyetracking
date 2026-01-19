5928d3783b25f72fa3c635c3c76f87c7398db55c
dev-jam <mail@bobrosbag.nl>
2026-01-19 14:47:55 +0100

    fix Error export data to tsv external signal #2

--- opensesame_plugins/titta/titta_save_data/titta_save_data.py
+++ opensesame_plugins/titta/titta_save_data/titta_save_data.py
@@ -40,21 +40,37 @@ class TittaSaveData(Item):
         self._check_stop()
         self.set_item_onset()
         self.experiment.tracker.save_data()
-        
-        if self.tsv_export == 'yes' and self.experiment.titta_dummy_mode == 'no':
-            df_gaze = pd.read_hdf(f'{self.experiment.titta_file_name}.h5', 'gaze')
-            df_msg = pd.read_hdf(f'{self.experiment.titta_file_name}.h5', 'msg')
-            df_external_signal = pd.read_hdf(f'{self.experiment.titta_file_name}.h5', 'external_signal')
-            df_calibration_history = pd.read_hdf(f'{self.experiment.titta_file_name}.h5', 'calibration_history')
-            df_merged = pd.concat([df_gaze, df_msg, df_external_signal])
-            df_merged.sort_values("system_time_stamp", axis = 0, ascending = True,
-                                 inplace = True, na_position ='last')
 
-            df_gaze.to_csv(f'{self.experiment.titta_file_name}_gaze.tsv', sep='\t')
-            df_msg.to_csv(f'{self.experiment.titta_file_name}_msg.tsv', sep='\t')
-            df_external_signal.to_csv(f'{self.experiment.titta_file_name}_external_signal.tsv', sep='\t')
-            df_calibration_history.to_csv(f'{self.experiment.titta_file_name}_calibration_history.tsv', sep='\t')
-            df_merged.to_csv(f'{self.experiment.titta_file_name}_data_merged.tsv', sep='\t')
+        if self.tsv_export == 'yes' and self.experiment.titta_dummy_mode == 'no':
+            items_to_process = {
+                'gaze': f'{self.experiment.titta_file_name}_gaze.tsv',
+                'msg': f'{self.experiment.titta_file_name}_msg.tsv',
+                'external_signal': f'{self.experiment.titta_file_name}_external_signal.tsv',
+                'calibration_history': f'{self.experiment.titta_file_name}_calibration_history.tsv'
+            }
+
+            loaded_dfs = {}
+
+            with pd.HDFStore(f'{self.experiment.titta_file_name}.h5', 'r') as store:
+                for item_name, output_file in items_to_process.items():
+                    if f'/{item_name}' in store.keys():
+                        loaded_dfs[item_name] = pd.read_hdf(store, item_name)
+                        loaded_dfs[item_name].to_csv(output_file, sep='\t')
+                        self._show_message(f"Saved {item_name} to {output_file}")
+                    else:
+                        self._show_message(f"Warning: '{item_name}' not found in HDF5 file")
+
+            merge_candidates = [loaded_dfs[key] for key in ['gaze', 'msg', 'external_signal']
+                               if key in loaded_dfs]
+
+            if merge_candidates:
+                df_merged = pd.concat(merge_candidates)
+                df_merged.sort_values("system_time_stamp", axis=0, ascending=True,
+                                     inplace=True, na_position='last')
+                df_merged.to_csv(f'{self.experiment.titta_file_name}_data_merged.tsv', sep='\t')
+                self._show_message("Saved merged data")
+            else:
+                self._show_message("Warning: No dataframes available to merge")
 
     def _check_init(self):
         if hasattr(self.experiment, "titta_dummy_mode"):
